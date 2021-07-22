7d121b827893ab6b25281c04a78f33aa8a1931f3
Reshma S <reshma97kaniyamparambil@gmail.com>
2021-07-22 12:45:27 +0530

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,12 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,6 +18,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             //  var users= _context.Users.ToList();
@@ -26,6 +26,7 @@ namespace API.Controllers
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         // api/users/3
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit{
       this.users=response;
     },
     error=>{
-      console.log(error);
+      console.log(error);4
     })
   }
 }
